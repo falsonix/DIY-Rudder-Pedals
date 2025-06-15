243d89dafc05aa474e18be73fe9a4cc093aed5ec
falsonix <greenemachine124@gmail.com>
2025-06-15 14:17:00 -0500

    implement library fully

--- code/initial-test/src/main.cpp
+++ code/initial-test/src/main.cpp
@@ -1,15 +1,20 @@
 #include <Arduino.h>
 #include <PicoGamepad.h>
 
+PicoGamepad gamepad;
 int potPin = ADC0;
 
 void setup() {
-  Serial.begin(115200);
   pinMode(potPin, INPUT);
 }
 
 void loop() {
   int pos = analogRead(potPin);
-  Serial.println(pos);
-  delay(500); // 0.5sec delay to allow for readability during testing
+  // from library's README: Map analog 0-1023 value from pin to max HID range -32767 - 32767
+  pos = map(pos, 0, 1023, -32767, 32767);
+  gamepad.SetX(pos);
+
+  gamepad.send_update();
+
+  delay(50); // 50ms delay for fast responses
 }
