2e807a6781977d3f4e0a2774960c8f2776c0bb80
falsonix <greenemachine124@gmail.com>
2025-06-16 17:24:24 -0500

    add support for all 3 axes, change refresh rate to 20ms

--- code/initial-test/src/main.cpp
+++ code/initial-test/src/main.cpp
@@ -2,19 +2,37 @@
 #include <PicoGamepad.h>
 
 PicoGamepad gamepad;
-int potPin = ADC0;
+int rightBrake = 26;
+int steering = 27;
+int leftBrake = 28;
 
 void setup() {
-  pinMode(potPin, INPUT);
+  Serial.begin(115200); // in case debugging is needed, hope this doesnt cause issues with COM allocation
+  pinMode(rightBrake, INPUT);
+  pinMode(steering, INPUT);
+  pinMode(leftBrake, INPUT);
 }
 
+// axis mappings (at least how i have them now)
+// rightBrake: axis 0
+// steering: axis 1
+// leftBrake: axis 2
+
 void loop() {
-  int pos = analogRead(potPin);
-  // from library's README: Map analog 0-1023 value from pin to max HID range -32767 - 32767
-  pos = map(pos, 0, 1023, -32767, 32767);
-  gamepad.SetX(pos);
+  int rightPos = analogRead(rightBrake);
+  // taken from the README of the PicoGamepad library, remap the raw ADC data to the full HID range
+  rightPos = map(rightPos, 0, 1023, -32767, 32767);
+  gamepad.SetAxis(0, rightPos);
+
+  int steeringPos = analogRead(steering);
+  steeringPos = map(steeringPos, 0, 1023, -32767, 32767);
+  gamepad.SetAxis(1, steeringPos);
+
+  int leftPos = analogRead(leftBrake);
+  leftPos = map(leftPos, 0, 1023, -32767, 32767);
+  gamepad.SetAxis(2, leftPos);
 
   gamepad.send_update();
 
-  delay(50); // 50ms delay for fast responses
+  delay(20);
 }
