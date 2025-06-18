3f12ec16bbb6467639f76834e436f6567adeeb06
falsonix <greenemachine124@gmail.com>
2025-06-18 18:12:34 -0500

    add automatic offset detection/zeroing for brakes

--- code/initial-test/src/main.cpp
+++ code/initial-test/src/main.cpp
@@ -5,12 +5,43 @@ PicoGamepad gamepad; // init gamepad object
 int rightBrake = 26;
 int steering = 27;
 int leftBrake = 28;
+int rightOffset = 0;
+int leftOffset = 0;
+
+// auto calibrate potentiometers in case physical alignment is not 100% perfect,
+// averages the amount of readings requested by checkTimes
+void calculateOffset(int potentiometer, int checkTimes) {
+  int measurements = 0;
+  for (int i = 0; i < checkTimes; i++) {
+    measurements += analogRead(potentiometer);
+    delay(2); // 2ms delay to account for any random fluctuations
+  }
+  int offset = measurements / checkTimes;
+
+  if (potentiometer == rightBrake) {
+    rightOffset = offset;
+  }
+  else if (potentiometer == leftBrake) {
+    leftOffset = offset;
+  }
+  else {
+    Serial.println("Potentiometer used in offset is invalid!");
+    Serial.print("Invalid potentiometer pin: ");
+    Serial.println(potentiometer);
+  }
+}
+
+// having the offsets like that might cause issues due to the potentiometers for each pedal being mirrored, reversing the values for one side
+// but oh well that's a problem for future me to fix
 
 void setup() {
   Serial.begin(115200); // in case debugging is needed, hope this doesnt cause issues with COM allocation (light testing reveals it's fine)
   pinMode(rightBrake, INPUT);
   pinMode(steering, INPUT);
   pinMode(leftBrake, INPUT);
+
+  calculateOffset(rightBrake, 4);
+  calculateOffset(leftBrake, 4);
 }
 
 // axis mappings (at least how i have them now)
@@ -19,7 +50,7 @@ void setup() {
 // leftBrake: axis 2
 
 void loop() {
-  int rightPos = analogRead(rightBrake);
+  int rightPos = (analogRead(rightBrake) - rightOffset);
   // taken from the README of the PicoGamepad library, remap the raw ADC data to the full HID range
   rightPos = map(rightPos, 0, 1023, -32767, 32767);
   gamepad.SetAxis(0, rightPos);
@@ -28,7 +59,7 @@ void loop() {
   steeringPos = map(steeringPos, 0, 1023, -32767, 32767);
   gamepad.SetAxis(1, steeringPos);
 
-  int leftPos = analogRead(leftBrake);
+  int leftPos = (analogRead(leftBrake) - leftOffset);
   leftPos = map(leftPos, 0, 1023, -32767, 32767);
   gamepad.SetAxis(2, leftPos);
 
